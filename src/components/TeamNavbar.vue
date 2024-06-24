a47b48ac0e8c72a5ed37bac5f7a586d9d2dede1c
Don-Debug <elision.q23@gmail.com>
2024-06-24 20:13:19 +0200

    start 3

---
+++ src/components/TeamNavbar.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header class="bg-yellow-300">
+    <div>
+      <img src="../assets/gqlteam.png" alt="site-logo" class="w-[10rem]" />
+    </div>
+  </header>
+</template>
+
+<style scoped></style>
